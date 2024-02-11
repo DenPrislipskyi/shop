2077d158bbfc847853e2d05f1dfffb809852a3ea
Denys <den.prislipskyi@gmail.com>
2024-02-11 19:26:13 +0100

    added device category tag

--- device/urls.py
+++ device/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from device.views import DeviceListView
+from device.views import DeviceListView, DeviceCategoryView
 
 urlpatterns = [
     path("", DeviceListView.as_view(), name="device-list"),
-    # path("category/", CategoryListView.as_view(), name="category-list"),
+    path("category/<int:pk>/", DeviceCategoryView.as_view(), name="device-category"),
 ]
 
 
--- manufacturer/views.py
+++
@@ -1,8 +0,0 @@
-from django.views import generic
-
-from manufacturer.models import Manufacturer
-
-
-class ManufacturerListView(generic.ListView):
-    model = Manufacturer
-    template_name = "shop/manufacturer_list.html"
