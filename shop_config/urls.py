658e4a9cae004599cfb5c8c5f46f4536e2ee6f28
Denys <den.prislipskyi@gmail.com>
2024-02-13 14:42:20 +0100

    added autenfication

--- shop_config/urls.py
+++ shop_config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("device/", include("device.urls", namespace="device")),
     path("cart/", include("cart.urls", namespace="cart")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
